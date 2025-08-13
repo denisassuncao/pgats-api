18ddd349ef0df56f4ac09a2e7a5d7d609c33d863
denisassuncao <denis.assuncao@hotmail.com>
2025-08-13 20:59:56 -0300

    Commit do primeiro teste escrito

---
+++ src/test/controller/transferController.test.js
@@ -0,0 +1,28 @@
+//biblioteca de testes
+const request = require('supertest');
+const {expect} = require('chai');
+const sinon = require('sinon');
+
+//Aplicação
+const app = require('../../app');
+
+//Teste do controlador
+describe('Transfer Controller', () => {
+    describe('POST /transfer', () => {
+        it('Quando informo remetente e destinatário inexistentes, retorna status 400', async() => {
+            const resposta = await request(app)
+            .post('/transfer')
+            .send({ 
+                from: "denis", 
+                to: "maria", 
+                value: 100
+            });
+            expect(resposta.status).to.equal(400);
+            expect(resposta.body.error).to.equal('Usuário não encontrado');
+        });
+    });
+
+        describe('POST /transfer', () => {
+        //teste
+    });
+});
